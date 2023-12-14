a1e41cb83a544c94b4af951efd01704930bcfb98
krlosl <caarlosluzz@gmail.com>
2023-12-14 14:59:37 +0100

    Add files via upload

---
+++ Segunda EV/pruebarecurs.java
@@ -0,0 +1,11 @@
+public static int fibonacci(int N) {
+    int res;
+    if (N == 0) {
+        res = 0;
+    } else if (N == 1) {
+        res = 1;
+    } else {
+        res = fibonacci(N - 1) * fibonacci(N - 2);
+    }
+    return res;
+}
